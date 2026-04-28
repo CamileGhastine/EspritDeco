1dcf3612bb12e20992a80ad9aca5f361a8be7bb4
CamileGhastine <camileghastine@hotmail.com>
2026-04-28 15:04:18 +0200

    affichage de tous les produits

--- app/src/Controller/ProductController.php
+++ app/src/Controller/ProductController.php
@@ -9,11 +9,16 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class ProductController extends AbstractController
 {
+    public function __construct(private ProductRepository $productRepository)
+    {
+    }
+
     #[Route('/', name: 'app_product_index')]
     public function index(): Response
     {
 
         return $this->render('product/index.html.twig', [
+            'products' => $this->productRepository->findALlWithCategoryAndPrincipalImage()
         ]);
     }
 }
---
+++ app/src/Repository/ProductRepository.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Product;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Product>
+ */
+class ProductRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Product::class);
+    }
+
+       /**
+        * @return Product[] Returns an array of Product objects
+        */
+       public function findALlWithCategoryAndPrincipalImage(): array
+       {
+           return $this->createQueryBuilder('p')
+                ->leftJoin('p.category', 'c')
+                ->addSelect('c')
+                ->leftJoin('p.images', 'i', 'WITH', 'i.isPrincipal = :isPrincipal')
+                ->addSelect('i')
+                ->setParameter('isPrincipal', true)
+                ->orderBy('p.id', 'ASC')
+                ->getQuery()
+                ->getResult()
+           ;
+       }
+
+    //    public function findOneBySomeField($value): ?Product
+    //    {
+    //        return $this->createQueryBuilder('p')
+    //            ->andWhere('p.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+}
