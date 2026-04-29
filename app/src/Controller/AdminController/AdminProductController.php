f663820caf402405b96d09fe39895ab37b0baaac
CamileGhastine <camileghastine@hotmail.com>
2026-04-29 15:32:43 +0200

    lot2-3: suppression produit

--- app/src/Controller/AdminController/AdminProductController.php
+++ app/src/Controller/AdminController/AdminProductController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller\AdminController;
 
+use App\Entity\Product;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,5 +18,30 @@ final class AdminProductController extends AbstractController
         return $this->render('admin/product/index.html.twig', [
             'products' => $productRepository->findAll(),
         ]);
+    }
+
+    #[Route('/admin/product/delete/{id<[0-9]+>}', name: 'app_admin_product_delete')]
+    public function delete(Product $product, Request $request, EntityManagerInterface $em): Response
+    {
+        $submittedToken = $request->getPayload()->get('token');
+
+        if (!$this->isCsrfTokenValid('delete-item-' . $product->getId(), $submittedToken)) {
+            $this->addFlash('danger', 'Token invalide.');
+
+            return $this->redirectToRoute('app_admin_product_index');
+        }
+
+        if (!$product) {
+            $this->addFlash('danger', 'Ce produit n\'existe pas.');
+
+            return $this->redirectToRoute('app_admin_product_index');
+        }
+
+        $em->remove($product);
+        $em->flush();
+
+        $this->addFlash('success', 'Le produit a été supprimé avec succès.');
+
+        return $this->redirectToRoute('app_admin_product_index');
     }
 }
