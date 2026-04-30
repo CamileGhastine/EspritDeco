d73879848058bf4849a51a20a46fd55db209880b
CamileGhastine <camileghastine@hotmail.com>
2026-04-30 15:16:16 +0200

    ajout de produit

---
+++ app/src/Service/ImageHandler.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Service;
+
+use App\Entity\Image;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class ImageHandler
+{
+    private Session $session;
+
+    public function __construct(
+        private SluggerInterface $slugger,
+        private RequestStack $requestStack
+        )
+    {
+        $this->session = $this->requestStack->getSession();
+    }
+
+    public function uploadImage(UploadedFile $imageFile, $product)
+    {
+        try {
+            $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+            $safeFilename = $this->slugger->slug($originalFilename);
+            $newFilename = 'img_' .uniqid() . '-' .$safeFilename . '.' . $imageFile->guessExtension();
+            $imageFile->move('images/product', $newFilename);
+            $image = new Image;
+            $image->setPath('images/product/' . $newFilename)
+                ->setAlt($originalFilename)
+                ->setIsPrincipal(true)
+                ->setProduct($product)
+            ;
+
+            return $image;
+        } catch (FileException $e) {
+            $this->session->getFlashBag()->add('danger', 'Erreur lors de l’upload de l’image.');
+            
+            return false;
+        }
+    }
+}
